0c1a402a9a010588cb9904da570b7e0349547ba2
@KumarDotnet <kumar67.1947@gmail.com>
2024-02-08 20:54:10 +0530

    Revert "style: password actionmethod and view added"

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -13,10 +13,5 @@ namespace SocialNetworking.Controllers
         {
             return View();
         }
-
-        public ActionResult Password()
-        {
-            return View();
-        }
     }
 }
